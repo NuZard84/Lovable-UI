3c52613d306b38e943fabb890abd458fa0a8bcfa
NuZrd84 <86848690+NuZard84@users.noreply.github.com>
2025-06-02 17:38:30 +0530

    add playground route

--- src/app/playground/page.tsx
+++ src/app/playground/page.tsx
@@ -1,15 +1,26 @@
-import { GridBackground } from "../docs/constants/content/code/GridBackground"
+// import { GridBackground } from "../docs/constants/content/code/GridBackground"
 
+// const DevTools =
+//     process.env.NODE_ENV === 'development'
+//         ? () => (
+//               <GridBackground full centered className="bg-white" overlay boxSize={42}>
+//                   <div className="h-10 w-20 bg-blue-500 rounded-lg flex justify-center items-center bg-wh">
+//                       <p className="text-center text-white">Button</p>
+//                   </div>
+//               </GridBackground>
+//           )
+//         : () => <div>404 | Not Found</div>
 
-const DevTools =
-    process.env.NODE_ENV === 'development'
-        ? () => (
-              <GridBackground full centered className="bg-white" overlay boxSize={42}>
-                  <div className="h-10 w-20 bg-blue-500 rounded-lg flex justify-center items-center bg-wh">
-                      <p className="text-center text-white">Button</p>
-                  </div>
-              </GridBackground>
-          )
-        : () => <div>404 | Not Found</div>
+// export default DevTools
 
-export default DevTools
+import { GridBackground } from "../docs/constants/content/code/GridBackground";
+
+const DevTools = () => (
+  <GridBackground full centered className="bg-white" overlay boxSize={42}>
+    <div className="h-10 w-20 bg-blue-500 rounded-lg flex justify-center items-center bg-wh">
+      <p className="text-center text-white">Button</p>
+    </div>
+  </GridBackground>
+);
+
+export default DevTools;
